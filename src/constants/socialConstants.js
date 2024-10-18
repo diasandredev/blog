db03390bf978866069f850cb793d1582d36f9b35
André Dias <andredds12@gmail.com>
2024-10-18 16:44:00 -0300

    fix cv

--- src/constants/socialConstants.js
+++ src/constants/socialConstants.js
@@ -8,5 +8,5 @@ export const SOCIAL_URL = {
   TWITTER: "https://twitter.com/diasduzurf",
   GITHUB: "https://github.com/diasandre",
   LINKEDIN: "https://www.linkedin.com/in/andr%C3%A9-dias-da-silva/",
-  CURRICULUM: "https://github.com/diasandre/my-blog-tech-stuff/raw/master/Andre%CC%81%20Dias%20da%20Silva%20Resume.pdf",
+  CURRICULUM: "https://drive.google.com/file/d/17y06Zwo6pUofwUFt7VZ5maFv49rlDc8e/view?usp=sharing",
 }
