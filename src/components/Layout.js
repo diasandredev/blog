e9b4e96889a2c205f5d15c915f7d22dc3f161a83
andredias <andredds12@gmail.com>
2025-12-26 23:08:46 -0300

    update title

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -30,7 +30,7 @@ const Layout = ({ children, paddingTop }) => {
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
                 <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet" />
-                <title>Tech Blog</title>
+                <title>diasandre blog</title>
             </Helmet>
 
             <MainWrapper>
